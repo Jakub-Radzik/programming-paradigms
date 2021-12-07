3a0c1dabbb7d349c603f6a24825537a856949da5
Jakub <jradzik4@gmail.com>
2021-12-07 15:35:45 +0100

    cw8, deklaracje 7,8

---
+++ Working Place/src/Cwiki/java/EmptyException.java
@@ -0,0 +1,11 @@
+package Cwiki.java;
+
+public class EmptyException extends Exception {
+
+    public EmptyException() {
+    }
+
+    public EmptyException(String message) {
+        super(message);
+    }
+}
